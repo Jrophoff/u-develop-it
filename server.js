6c8001892eeecc9c174564e9f99508af59c0725f
Jordan Ophoff <noreply>
2021-12-14 23:59:36 -0500

    Connected to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,21 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'bRLGUc-ly$t4?Yu0E#!U',
+        database: 'election'
+    },
+    console.log('Connected to the elction database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // default reesponse for any other request (Not Found)
 app.use ((req, res) => {
